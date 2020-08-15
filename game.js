4824d672d7b444d6e8c7555139b4978e2c8ce3e7
Bean Dream <beandreamed@gmail.com>
2020-08-15 10:15:48 -0400

    ItS progrees but doesnt work.
    I want to rewrite it but i want to be able to go back to this incase i break everything

--- game.js
+++ game.js
@@ -1,6 +1,120 @@
 module.exports = {
     loadGame: function (msg) {
-        console.log("Game Is Loaded!");
-        console.log(msg);
+        console.log("Game Starting!");
+        setup(msg);
     }
+}
+
+var grid = [];
+var gameMessage;
+
+
+function setup(msg) {
+    gameMessage = null;
+    grid = createGrid();
+    grid = updateGrid(grid, 2, 2, "⬜");
+    sendGameEmbed(msg, convertGrid(grid));
+}
+
+function sendGameEmbed(msg, txtgrid) {
+    if (!gameMessage){
+        msg.channel.send(createEmbed(false, false, txtgrid)).then(bmsg => {
+            gameMessage = bmsg;
+            reactMessage(gameMessage);
+        });
+    } else {
+        gameMessage.edit(createEmbed(false, false, txtgrid)).then(bmsg => {
+            gameMessage = bmsg;
+            reactMessage(gameMessage);
+        });
+    }
+}
+
+function reactMessage(msg) {
+    const filter = (reaction, user) => {
+        return ['👍', '👎'].includes(reaction.emoji.name) && user.id === msg.author.id;
+    };
+
+    msg.react('👍').then(() => msg.react('👎')).then(() => {
+        awaitReactions(msg, filter);
+    });
+}
+
+function awaitReactions(msg, filter) {
+    msg.awaitReactions(filter, { max: 1, time: 60000, errors: ['time'] })
+	.then(collected => {
+		const reaction = collected.first();
+
+		if (reaction.emoji.name === '👍') {
+            msg.reactions.removeAll().catch(error => console.error('Failed to clear reactions: ', error));
+            moveCube();
+            console.log("YOOO");
+            
+		} else {
+            // msg.reactions.removeAll().catch(error => console.error('Failed to clear reactions: ', error));
+            // moveCube();
+        }
+        console.log("YOOOOOOO!")
+	})
+	.catch(collected => {
+		msg.reply('you reacted with neither a thumbs up, nor a thumbs down.');
+	});
+}
+
+function createEmbed(title, color, description) {
+    if (!title){title = "Discord Bot Game"};
+    if (!color) {color = 7339251};
+    if (!description) { "GameLoading" };
+    var embed = {
+        "title": title,
+        "color": color,
+        "description":description
+    }
+    return {embed};
+}
+
+function updateGrid(grid, y, x, type) {
+    var lines = grid;
+    if (!type){type = "⬛";}
+
+    for (var l = 0; l < lines.length; l++) {
+        for(var b = 0; b < lines[l].length; b++) {
+            if (y == l && x == b) {
+                lines[l][b] = type;
+           }
+        }
+    }
+    return lines;
+}
+
+function createGrid() {
+    var lines = [];
+    for (var l = 0; l < 12; l++) {
+        var blocks = [];
+        for(var b = 0; b < 12; b++) {
+            blocks.push("⬛");
+        }
+        lines.push(blocks);
+    }
+    return lines;
+}
+
+function convertGrid(lines) {
+    var text = "";
+    lines.forEach(y => {
+        y.forEach(x => {
+            text += x;
+        })
+        text += "\n"
+    });
+    return text;
+}
+
+function moveCube() {
+    console.log("SAY SOMETHN G");
+    grid = createGrid();
+    var x = Math.floor(Math.random() * 10);
+    var y = Math.floor(Math.random() * 10);
+    grid = updateGrid(grid, y, x, "⬜");
+    sendGameEmbed(msg, convertGrid(grid));
 }
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const Game = require('./game.js');
+var key = require('./key.js');
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
@@ -9,9 +10,8 @@ client.on('ready', () => {
 client.on('message', msg => {
     if (msg.bot) {return}
     if (msg.content === '!play') {
-        msg.reply('Ok! Loading Game!');
         Game.loadGame(msg);
     }
 });
 
-client.login('NDMzNzY0NjEwMTE0MzIyNDUy.Ws6Uxg.AyiT-LXnv4aQiJazCmhjbrQdvd0');
+client.login(key.key());
